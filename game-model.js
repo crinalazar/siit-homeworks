d9ee4205631e7dc1f544c167f970be148a2be4bd
Lazar Crina <crinalazar.a@gmail.com>
2020-03-12 21:14:31 +0200

    first update

--- game-model.js
+++ game-model.js
@@ -1,13 +1,17 @@
-class Game{
-    url = 'http://games-world.herokuapp.com/games';
+class GameModel{
 
+    constructor(){
+    this.url = 'http://games-world.herokuapp.com/games';
+    }
+    
     getAll() {
         return fetch(this.url).then(res => res.json());
-    }
+    };
 
-    findById(id) {
+    findById(id){
         return fetch(`${this.url}/${id}`).then(res => res.json());
     }
+
 
     handleEdit(id) {
         fetch(`${this.url}/${id}`, {
@@ -16,14 +20,13 @@ class Game{
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: 'title=new title'
-        })
+        });
     };
 
     handleDelete(id) {
         fetch(`${this.url}/${id}`, {
             method: 'DELETE'
-        })
-
+        });
     };
 
     handleCreate(){
@@ -34,9 +37,8 @@ class Game{
             },
             body: 'title=Watch_Dogs 2&imageUrl=https://psmedia.playstation.com/is/image/psmedia/watch-dogs-2-two-column-01-ps4-eu-09jun16?$TwoColumn_Image$ &description=Take an extended tour of San Francisco and test your skills in a range of high-risk additional missions and contracts with the Watch_Dogs 2 Season Pass.Take T-Bone s custom car-flipping truck for a spin in a special mission included in the first DLC pack, embark on three hour-long story missions with Lenni and Jordan in the second DLC pack, Human Conditions, and explore the city s seedy sex trade in the final DLC pack, No Compromises.'
         })
-    };
+    }
 }
 
 
-// export default Game;
 
